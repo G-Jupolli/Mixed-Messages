78123285aa4ccc69a4d4aaf82bdcd840e451f650
G-Jupolli <91795606+G-Jupolli@users.noreply.github.com>
2021-10-04 18:24:37 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+const funcCardNum = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+const funcCardSuit = ['Hearts', 'Spades', 'Clubs', 'Dimonds'];
+const funcDealer= ['Dave', 'Mia', 'Luca', 'Ivy', 'Quinn', 'Atlas','Lucas', 'Alice', 'Skylar', 'Parker'];
+
+
+const Hand = () => {
+	let card1 = funcCardNum[Math.floor(Math.random() * funcCardNum.length)] + ' of ' + funcCardSuit[Math.floor(Math.random() * funcCardSuit.length)];
+	let card2 = funcCardNum[Math.floor(Math.random() * funcCardNum.length)] + ' of ' + funcCardSuit[Math.floor(Math.random() * funcCardSuit.length)];
+	while (card1 === card2) {
+		card2 = funcCardNum[Math.floor(Math.random() * funcCardNum.length)] + ' of ' + funcCardSuit[Math.floor(Math.random() * funcCardSuit.length)];
+	}
+	let dealer = funcDealer[Math.floor(Math.random() * funcDealer.length)];
+	return `${dealer} has dealt you the hand: ${card1} & ${card2}`;
+}
+
+console.log(hand)
